6a0a5faf99deb08993911179216bfb99cb4e4f29
Mahdieh2001 <112332619+Mahdieh2001@users.noreply.github.com>
2024-06-20 23:13:59 +0330

    page2

--- script.js
+++ script.js
@@ -12,20 +12,82 @@ function moveCircles() {
     const linkedin = document.querySelector('.linkedin');
     const whatsapp = document.querySelector('.whatsapp');
     const instagram = document.querySelector('.instagram');
+    const circleRR = document.querySelector('.circleRR');
+    const circleLL = document.querySelector('.circleLL');
 
-    // Add animation to move the logo up
-    circleR.style.animation = 'moveRight 2s forwards';
-    circleL.style.animation = 'moveLeft 2s forwards';
+    circleR.style.animation = 'moveRight 3s forwards';
+    circleL.style.animation = 'moveLeft 3s forwards';
     logo.style.animation = 'moveLogoUp 2s forwards';
     logoo.style.animation = 'moveLogooUp 2s forwards';
     t1.style.animation = 'movet1Up 2s forwards';
     t2.style.animation = 'movet2Up 2s forwards';
-    p1.style.animation = 'showp1 4s forwards';
-    p2.style.animation = 'showp2 4s forwards';
-    p3.style.animation = 'showp3 4s forwards';
+    p1.style.animation = 'showp1 3s forwards';
+    p2.style.animation = 'showp2 3s forwards';
+    p3.style.animation = 'showp3 3s forwards';
     linkedin.style.animation = 'showlinkedin 1.5s forwards';
     whatsapp.style.animation = 'showwhatsapp 1.5s forwards';
     instagram.style.animation = 'showinstagram 1.5s forwards';
+    circleRR.style.animation = 'showR 3s forwards';
+    circleLL.style.animation = 'showL 3s forwards';
+
 }
 
-window.onresize = function(){ location.reload(); }
+function moveCircleLL() {
+    const circleLL = document.querySelector('.circleLL');
+    const circleRR = document.querySelector('.circleRR');
+    const logo = document.querySelector('.logo');
+    const logoo = document.querySelector('.logoo');
+    const t1 = document.querySelector('.t1');
+    const t2 = document.querySelector('.t2');
+    const p1 = document.querySelector('.p1');
+    const p2 = document.querySelector('.p2');
+    const p3 = document.querySelector('.p3');
+    const linkedin = document.querySelector('.linkedin');
+    const whatsapp = document.querySelector('.whatsapp');
+    const instagram = document.querySelector('.instagram');
+    // const home = document.querySelector('.home');
+
+    circleLL.style.animation = 'moveLL 2s forwards';
+    circleRR.style.animation = 'moveRR 2s forwards';
+    logo.style.animation = 'moveLogo 2s forwards';
+    logoo.style.animation = 'moveLogoo 2s forwards';
+    t1.style.animation = 'movet1 2s forwards';
+    t2.style.animation = 'movet2 2s forwards';
+    p1.style.animation = 'movep1 2s forwards';
+    p2.style.animation = 'movep2 2s forwards';
+    p3.style.animation = 'movep3 2s forwards';
+    linkedin.style.animation = 'movelinkedin 2s forwards';
+    whatsapp.style.animation = 'movewhatsapp 2s forwards';
+    instagram.style.animation = 'moveinstagram 2s forwards';
+    // home.style.animation = 'moveH 2s forwards';
+}
+
+function moveCircleRR() {
+    const circleLLR = document.querySelector('.circleLL');
+    const circleRRR = document.querySelector('.circleRR');
+    const logo = document.querySelector('.logo');
+    const logoo = document.querySelector('.logoo');
+    const t1 = document.querySelector('.t1');
+    const t2 = document.querySelector('.t2');
+    const p1 = document.querySelector('.p1');
+    const p2R = document.querySelector('.p2');
+    const p3R = document.querySelector('.p3');
+    const linkedin = document.querySelector('.linkedin');
+    const whatsapp = document.querySelector('.whatsapp');
+    const instagram = document.querySelector('.instagram');
+    // const home = document.querySelector('.home');
+
+    circleLLR.style.animation = 'moveLLR 2s forwards';
+    circleRRR.style.animation = 'moveRRR 2s forwards';
+    logo.style.animation = 'moveLogo 2s forwards';
+    logoo.style.animation = 'moveLogoo 2s forwards';
+    t1.style.animation = 'movet1 2s forwards';
+    t2.style.animation = 'movet2 2s forwards';
+    p1.style.animation = 'movep1 2s forwards';
+    p2R.style.animation = 'movep2R 2s forwards';
+    p3R.style.animation = 'movep3R 2s forwards';
+    linkedin.style.animation = 'movelinkedin 2s forwards';
+    whatsapp.style.animation = 'movewhatsapp 2s forwards';
+    instagram.style.animation = 'moveinstagram 2s forwards';
+    // home.style.animation = 'moveH 2s forwards';
+}
